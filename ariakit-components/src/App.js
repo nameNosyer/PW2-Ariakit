1e4c78ccee0c7a2ce1358512bc0d16546d9c0cee
nameNosyer <reysonferreira2018@gmail.com>
2023-07-06 13:03:26 -0300

    Add

---
+++ ariakit-components/src/App.js
@@ -0,0 +1,13 @@
+import Form from './components/form/form'
+import Disclosure from './components/disclosure/disclosure'
+
+function App() {
+  return (
+    <div>
+      <Form/>
+      <Disclosure/>
+    </div>
+  );
+}
+
+export default App;
